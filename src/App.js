5030deaa82388324956c9364c05284d3f32ea9e8
Bikki Mahato <bikkimahato123@gmail.com>
2022-08-07 22:28:15 +0530

    Added CryptoDetails Page and LineChart

--- src/App.js
+++ src/App.js
@@ -35,8 +35,8 @@ const App = () => {
             level={5}
             style={{ color: "white", textAlign: "center" }}
           >
-            Copyright © 2021
-            <Link to="/">Cryptoverse Inc.</Link> <br />
+            Copyright © 2022
+            <Link to="/"> Cryptoverse Inc.</Link> <br />
             All Rights Reserved.
           </Typography.Title>
           <Space>
